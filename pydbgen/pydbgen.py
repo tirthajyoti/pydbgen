5594e91642fa3abf29f6f2110a26eefa5d469a62
Havan <havanagrawal@gmail.com>
2018-03-18 00:21:03 -0700

    Clean up imports, spaces and refactor functions

--- pydbgen/pydbgen.py
+++ pydbgen/pydbgen.py
@@ -1,3 +1,8 @@
+import os
+import random
+from random import randint, choice
+import pandas as pd
+
 class pydb():
 
     def __init__(self, seed=None):
@@ -6,16 +11,15 @@ class pydb():
         seed: User can set a seed parameter to generate deterministic, non-random output
         """
         from faker import Faker
-        from random import randint
 
         self.fake = Faker()
         self.seed = seed
         self.randnum = randint(1,9)
 
         self.city_list = self._initialize_city_list()
+        self.domain_list = self._initialize_email_domain_list()
 
     def _initialize_city_list(self):
-        import os
         from six import moves
         import ssl
 
@@ -30,25 +34,32 @@ class pydb():
 
         return city_list
 
+    def _initialize_email_domain_list(self):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        path = dir_path + os.sep + "Domains.txt"
+
+        domain_list = []
+        with open(path) as fh:
+            domain_list = [str(line).strip() for line in fh.readlines()]
+
+        return domain_list
 
     def simple_ph_num(self, seed=None):
         """
         Generates 10 digit US phone number in xxx-xxx-xxxx format
         seed: Currently not used. Uses seed from the pydb class if chosen by user
         """
-        import random
-        from random import randint,choice
         random.seed(self.seed)
 
         result = str(randint(1,9))
         for _ in range(2):
-            result+=str(randint(0,9))
-        result+='-'
+            result += str(randint(0,9))
+        result += '-'
         for _ in range(3):
-            result+=str(randint(0,9))
-        result+='-'
+            result += str(randint(0,9))
+        result += '-'
         for _ in range(4):
-            result+=str(randint(0,9))
+            result += str(randint(0,9))
         return result
 
 
@@ -62,65 +73,50 @@ class pydb():
         If style is not specified by user, a random style is chosen at runtime
         seed: Currently not used. Uses seed from the pydb class if chosen by user
         """
-        import random
-        from random import randint, choice
         random.seed(self.seed)
 
         if not style:
             style = choice([1,2,3])
 
-        if style==1:
+        if style == 1:
             result = str(randint(1,9))
             for _ in range(3):
                 result+=chr(randint(65,90))
             for _ in range(3):
                 result+=str(randint(1,9))
             return result
-        elif style==2:
-            result=''
+        elif style == 2:
+            result = ''
             for _ in range(3):
-                result+=chr(randint(65,90))
-            result+='-'
+                result += chr(randint(65,90))
+            result += '-'
             for _ in range(4):
-                result+=str(randint(0,9))
+                result += str(randint(0,9))
             return result
         else:
-            result=''
+            result = ''
             for _ in range(3):
-                result+=chr(randint(65,90))
-            result+='-'
+                result += chr(randint(65,90))
+            result += '-'
             for _ in range(3):
-                result+=str(randint(0,9))
+                result += str(randint(0,9))
             return result
 
 
-    def realistic_email(self,name,seed=None):
+    def realistic_email(self, name, seed=None):
         '''
         Generates realistic email from first and last name and a random domain address
         seed: Currently not used. Uses seed from the pydb class if chosen by user
         '''
-        import random
-        import os
-        from random import randint,choice
         random.seed(self.seed)
 
         name = str(name)
-        result = ''
         f_name = name.split()[0]
         l_name = name.split()[-1]
 
         choice_int = choice(range(10))
 
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        path = dir_path + os.sep + "Domains.txt"
-
-        domain_list = []
-        fh = open(path)
-        for line in fh.readlines():
-            domain_list.append(str(line).strip())
-
-        domain = choice(domain_list)
-        fh.close()
+        domain = choice(self.domain_list)
 
         name_formats = ["{f}{last}", "{first}{last}",
                         "{first}.{l}", "{first}_{l}",
@@ -129,21 +125,19 @@ class pydb():
         name_fmt_choice = choice(name_formats)
         name_combo = name_fmt_choice.format(f=f_name[0], l=l_name[0], first=f_name, last=l_name)
 
-        if (choice_int<7):
-            result+=name_combo+'@'+str(domain)
+        if choice_int < 7:
+            email = name_combo + '@' + str(domain)
         else:
-            random_int = randint(11,99)
-            result+=name_combo+str(random_int)+'@'+str(domain)
+            random_int = randint(11, 99)
+            email = name_combo + str(random_int) + '@' + str(domain)
 
-        return result
+        return email
 
-    def city_real(self,seed=None):
+    def city_real(self, seed=None):
         '''
         Picks and returns a random entry out of 385 US cities
         seed: Currently not used. Uses seed from the pydb class if chosen by user
         '''
-        import random
-        from random import randint, choice
         random.seed(self.seed)
 
         return choice(self.city_list)
@@ -175,11 +169,9 @@ class pydb():
         if num <= 0:
             raise ValueError('Number of samples must be a positive integer, found ' + num)
 
-        import pandas as pd
         num = int(num)
         fake = self.fake
         fake.seed(self.seed)
-        lst = []
 
         func_lookup = {
             'name': fake.name,
@@ -224,7 +216,7 @@ class pydb():
         if num_cols < 0:
             raise ValueError("Please provide at least one type of data field to be generated")
 
-    def gen_dataframe(self,num=10,fields=['name'], real_email=True, real_city=True, phone_simple=True, seed=None):
+    def gen_dataframe(self, num=10, fields=['name'], real_email=True, real_city=True, phone_simple=True, seed=None):
         """
         Generate a pandas dataframe filled with random entries.
         User can specify the number of rows and data type of the fields/columns
@@ -245,29 +237,26 @@ class pydb():
         """
         self._validate_args(num, fields)
 
-        import pandas as pd
-        from random import randint,choice
-
-        df = pd.DataFrame(data=self.gen_data_series(num,data_type=fields[0]),columns=[fields[0]])
+        df = pd.DataFrame(data=self.gen_data_series(num, data_type=fields[0]), columns=[fields[0]])
         for col in fields[1:]:
-            if col=='phone':
-                if phone_simple==True:
-                    df['phone-number']=self.gen_data_series(num,data_type='phone_number_simple')
+            if col == 'phone':
+                if phone_simple:
+                    df['phone-number'] = self.gen_data_series(num, data_type='phone_number_simple')
                 else:
-                    df['phone-number']=self.gen_data_series(num,data_type='phone_number_full')
-            elif col=='license_plate':
-                df['license-plate']=self.gen_data_series(num,data_type=col)
-            elif col=='city' and real_city==True:
-                df['city']=self.gen_data_series(num,data_type='real_city')
+                    df['phone-number'] = self.gen_data_series(num, data_type='phone_number_full')
+            elif col == 'license_plate':
+                df['license-plate'] = self.gen_data_series(num, data_type=col)
+            elif col == 'city' and real_city:
+                df['city'] = self.gen_data_series(num, data_type='real_city')
             else:
-                df[col]=self.gen_data_series(num,data_type=col)
+                df[col] = self.gen_data_series(num, data_type=col)
 
-        if ('email' in fields) and ('name' in fields) and (real_email==True):
-                df['email']=df['name'].apply(self.realistic_email)
+        if ('email' in fields) and ('name' in fields) and real_email:
+                df['email'] = df['name'].apply(self.realistic_email)
 
         return df
 
-    def gen_table(self,num=10,fields=['name'],db_file=None,table_name=None,primarykey=None,real_email=True, real_city=True, phone_simple=True, seed=None):
+    def gen_table(self, num=10, fields=['name'], db_file=None, table_name=None, primarykey=None, real_email=True, real_city=True, phone_simple=True, seed=None):
         """
         Attempts to create a table in a database (.db) file using Python's built-in SQLite engine.
         User can specify various data types to be included as database table fields.
@@ -293,23 +282,23 @@ class pydb():
         self._validate_args(num, fields)
 
         import sqlite3
-        if db_file==None:
+        if not db_file:
             conn = sqlite3.connect('NewFakeDB.db')
-            c=conn.cursor()
+            c = conn.cursor()
         else:
             conn = sqlite3.connect(str(db_file))
-            c=conn.cursor()
+            c = conn.cursor()
 
-        if type(primarykey)!=str and primarykey!=None:
+        if type(primarykey) != str and primarykey is not None:
             print("Primary key type not identified. Not generating any table")
             return None
 
         # If primarykey is None, designate the first field as primary key
-        if primarykey==None:
-            table_cols='('+str(fields[0])+' varchar PRIMARY KEY NOT NULL,'
+        if not primarykey:
+            table_cols = '(' + str(fields[0]) + ' varchar PRIMARY KEY NOT NULL,'
             for col in fields[1:-1]:
-                table_cols+=str(col)+' varchar,'
-            table_cols+=str(fields[-1])+' varchar'+')'
+                table_cols += str(col) + ' varchar,'
+            table_cols += str(fields[-1]) + ' varchar' + ')'
             #print(table_cols)
         else:
             pk = str(primarykey)
@@ -317,19 +306,19 @@ class pydb():
                 print("Desired primary key is not in the list of fields provided, cannot generate the table!")
                 return None
 
-            table_cols='('+str(fields[0])+' varchar, '
+            table_cols = '('+str(fields[0]) + ' varchar, '
             for col in fields[1:-1]:
-                if col==pk:
-                    table_cols+=str(col)+' varchar PRIMARY KEY NOT NULL,'
+                if col == pk:
+                    table_cols += str(col) + ' varchar PRIMARY KEY NOT NULL,'
                 else:
-                    table_cols+=str(col)+' varchar, '
-            table_cols+=str(fields[-1])+' varchar'+')'
+                    table_cols += str(col)+' varchar, '
+            table_cols += str(fields[-1])+' varchar'+')'
             #print(table_cols)
 
-        if table_name==None:
-            table_name='Table1'
+        if not table_name:
+            table_name = 'Table1'
         else:
-            table_name=table_name
+            table_name = table_name
 
         str_drop_table = "DROP TABLE IF EXISTS " + str(table_name) + ';'
         c.execute(str_drop_table)
@@ -338,17 +327,17 @@ class pydb():
         c.execute(str_create_table)
 
         # Create a temporary df
-        temp_df = self.gen_dataframe(num=num,fields=fields,real_email=real_email,real_city=real_city,phone_simple=phone_simple)
+        temp_df = self.gen_dataframe(num=num, fields=fields, real_email=real_email, real_city=real_city, phone_simple=phone_simple)
         # Use the dataframe to insert into the table
         for i in range(num):
-            str_insert="INSERT INTO "+table_name+ " VALUES "+ str(tuple(temp_df.iloc[i])) +';'
+            str_insert = "INSERT INTO " + table_name + " VALUES " + str(tuple(temp_df.iloc[i])) + ';'
             c.execute(str_insert)
 
         # Commit the insertions and close the connection
         conn.commit()
         conn.close()
 
-    def gen_excel(self,num=10, fields=['name'],filename=None,real_email=True, real_city=True, phone_simple=True, seed=None):
+    def gen_excel(self, num=10, fields=['name'], filename='NewExcel.xlsx', real_email=True, real_city=True, phone_simple=True, seed=None):
         """
         Attempts to create an Excel file using Pandas excel_writer function.
         User can specify various data types to be included as fields.
@@ -371,12 +360,7 @@ class pydb():
         """
         self._validate_args(num, fields)
 
-        if filename==None:
-            fname='NewExcel.xlsx'
-        else:
-            fname=filename
-
         # Create a temporary dataframe
-        temp_df=self.gen_dataframe(num=num,fields=fields,real_email=real_email,real_city=real_city,phone_simple=phone_simple)
+        temp_df = self.gen_dataframe(num=num, fields=fields, real_email=real_email, real_city=real_city, phone_simple=phone_simple)
          # Use the dataframe to write to an Excel file using Pandas built-in function
-        temp_df.to_excel(fname)
+        temp_df.to_excel(filename)
