5fc211a717ff2d40e39831ac95e302c5337d6db4
Tirthajyoti Sarkar <tirthajyoti@gmail.com>
2019-07-24 01:51:00 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -23,3 +23,11 @@ author = 'Tirthajyoti Sarkar'
 
 # The full version, including alpha/beta/rc tags
 release = '1.0.5'
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+
+master_doc = 'index'
