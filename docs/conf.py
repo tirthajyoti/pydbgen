b00618632bc22e064fc3a4eea9ee27eda2cf5298
Tirthajyoti Sarkar <tirthajyoti@gmail.com>
2019-07-24 01:42:42 -0700

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,25 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# http://www.sphinx-doc.org/en/master/config
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'pydbgen'
+copyright = '2019, Tirthajyoti Sarkar'
+author = 'Tirthajyoti Sarkar'
+
+# The full version, including alpha/beta/rc tags
+release = '1.0.5'
